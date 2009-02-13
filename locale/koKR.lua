72a8f15e4f498f915310f57a43b6a6c4b7805feb
7destiny <7destiny@users.noreply.wowace.com>
2009-02-13 20:59:55 +0000

    add koKR locale

---
+++ locale/koKR.lua
@@ -0,0 +1,12 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Skada", "koKR", false)
+
+L["Disable"] = "비활성화"
+L["Profiles"] = "프로필"
+L["Hint: Left-Click to toggle Skada window."] = "힌트: 좌-클릭으로 Skada 창을 전환합니다."
+L["Shift + Left-Click to reset."] = "Shift + 좌-클릭으로 초기화 합니다."
+L["Right-click to configure"] = "우-클릭 설정"
+L["Options"] = "옵션"
+L["Appearance"] = "발표"
+L["A damage meter."] = "데미지 미터입니다."
+L["Skada summary"] = "Skada 요약"
+
