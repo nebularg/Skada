abe5bb89a036f9d0cb13b6928b22d927bb619a08
Zarnivoop <Zarnivoop@users.noreply.wowace.com>
2009-02-14 19:51:45 +0000

    Added raw threat (unmodified by range) display as an option. Had to use threatvalue since rawthreatpct seemed a bit wacky.

--- modules/SkadaThreat/Threat.lua
+++ modules/SkadaThreat/Threat.lua
@@ -5,6 +5,27 @@ local mod = Skada:NewModule("ThreatMode")
 
 mod.name = L["Threat"]
 
+local options = {
+	type="group",
+	name=L["Threat"],
+	args={
+		rawthreat = {
+			type = "toggle",
+			name = L["Show raw threat"],
+			desc = L["Shows raw threat percentage relative to tank instead of modified for range."],
+			get = function() return Skada.db.profile.rawthreat end,
+			set = function() Skada.db.profile.rawthreat = not Skada.db.profile.rawthreat end,
+			order=1,
+		},
+	},
+}
+
+function mod:OnInitialize()
+	-- Add our options.
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("Skada-Threat", options)
+	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Skada-Threat", "Threat", "Skada")
+end
+
 function mod:OnEnable()
 	-- Listen for target changes
 	
@@ -17,6 +38,27 @@ function mod:OnDisable()
 	Skada:RemoveMode(self)
 end
 
+local maxthreat = 0
+
+local function add_to_threattable(name, tbl)
+	local isTanking, status, threatpct, rawthreatpct, threatvalue = UnitDetailedThreatSituation(name, "target")
+
+	if Skada.db.profile.rawthreat then
+		if threatvalue then
+			local class = select(2, UnitClass(name))
+			table.insert(tbl, {["name"] = name, ["threat"] = threatvalue, ["class"] = class})
+			if threatvalue > maxthreat then
+				maxthreat = threatvalue
+			end
+		end
+	else
+		if threatpct then
+			local class = select(2, UnitClass(name))
+			table.insert(tbl, {["name"] = name, ["threat"] = threatpct, ["class"] = class})
+		end
+	end
+end
+
 function mod:Update(set)
 	if not UnitExists("target") then
 		-- We have no target - wipe all threat bars.
@@ -25,16 +67,17 @@ function mod:Update(set)
 	end
 	
 	local threattable = {}
+	maxthreat = 0
 	
 	if GetNumRaidMembers() > 0 then
 		-- We are in a raid.
 		for i = 1, 40, 1 do
 			local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
 			if name then
-				local isTanking, status, threatpct, rawthreatpct, threatvalue = UnitDetailedThreatSituation(name, "target")
-			
-				if threatpct then
-					table.insert(threattable, {["name"] = name, ["threat"] = threatpct})
+				add_to_threattable(name, threattable)
+				
+				if UnitExists("raid"..i.."pet") then
+					add_to_threattable(select(1, UnitName("raid"..i.."pet")), threattable)
 				end
 			end
 		end
@@ -43,28 +86,20 @@ function mod:Update(set)
 		for i = 1, 5, 1 do
 			local name = (UnitName("party"..tostring(i)))
 			if name then
-				local isTanking, status, threatpct, rawthreatpct, threatvalue = UnitDetailedThreatSituation(name, "target")
+				add_to_threattable(name, threattable)
 
-				if threatpct then
-					table.insert(threattable, {["name"] = name, ["threat"] = threatpct})
+				if UnitExists("raid"..i.."pet") then
+					add_to_threattable(select(1, UnitName("party"..i.."pet")), threattable)
 				end
 			end
 		end
 	else
 		-- We are all alone.
-		local isTanking, status, threatpct, rawthreatpct, threatvalue = UnitDetailedThreatSituation("player", "target")
-		
-		if threatpct then
-			table.insert(threattable, {["name"] = select(1, UnitName("player")), ["threat"] = threatpct})
-		end
+		add_to_threattable(UnitName("player"), threattable)
 		
 		-- Maybe we have a pet?
 		if UnitExists("pet") then
-			local isTanking, status, threatpct, rawthreatpct, threatvalue = UnitDetailedThreatSituation("pet", "target")
-			
-			if threatpct then
-				table.insert(threattable, {["name"] = select(1, UnitName("pet")), ["threat"] = threatpct})
-			end
+			add_to_threattable(UnitName("pet"), threattable)
 		end
 	end
 
@@ -76,21 +111,26 @@ function mod:Update(set)
 		end
 	end
 
+	-- If we are going by raw threat we got the max threat from above; otherwise it's always 100.
+	if not Skada.db.profile.rawthreat then
+		maxthreat = 100
+	end
+	
 	-- For each player in threat table, create or update bars.
 	for i, player in ipairs(threattable) do
 		if player.threat > 0 then
 			local bar = Skada:GetBar(player.name)
 			if bar then
-				bar:SetMaxValue(100)
+				bar:SetMaxValue(maxthreat)
 				bar:SetValue(player.threat)
 			else
-				bar = Skada:CreateBar(player.name, player.name, player.threat, 100, nil, false)
+				bar = Skada:CreateBar(player.name, player.name, player.threat, maxthreat, nil, false)
 				bar:EnableMouse()
 				bar:SetScript("OnMouseDown", function(bar, button) if button == "RightButton" then Skada:RightClick() end end)
 				local color = Skada.classcolors[player.class] or Skada:GetDefaultBarColor()
 				bar:SetColorAt(0, color.r, color.g, color.b, color.a)
 			end
-			bar:SetTimerLabel(("%02.1f%%"):format(player.threat).."%")
+			bar:SetTimerLabel(("%02.1f%%"):format(player.threat / maxthreat * 100).."%")
 			bar.checked = true
 		end
 	end
