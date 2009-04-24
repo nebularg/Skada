57fd05f3ca82d7b3cf199b24c613c8f3ef95f599
zarnivoop <zarnivoop@users.noreply.wowace.com>
2009-04-24 15:04:35 +0000

    Added a mana gain mode.

---
+++ modules/SkadaPower/Power.lua
@@ -0,0 +1,166 @@
+local L = LibStub("AceLocale-3.0"):GetLocale("Skada", false)
+
+local Skada = Skada
+
+local mod = Skada:NewModule("PowerGainedMode")
+
+mod.name = L["Mana gained"]
+
+local function log_gain(set, gain)
+	-- Get the player from set.
+	local player = Skada:get_player(set, gain.playerid, gain.playername)
+	if player then
+		-- Make sure power type exists.
+		if not player.power[gain.type] then
+			player.power[gain.type] = {spells = {}, amount = 0}
+		end
+		
+		-- Make sure set power type exists.
+		if not set.power[gain.type] then
+			set.power[gain.type] = 0
+		end
+	
+		-- Add to player total.
+		player.power[gain.type].amount = player.gains[gain.type].amount + gain.amount
+		
+		-- Also add to set total gain.
+		set.power[gain.type] = set.power[gain.type] + gain.amount
+		
+		-- Create spell if it does not exist.
+		if not player.power[gain.type].spells[gain.spellname] then
+			player.power[gain.type].spells[gain.spellname] = 0
+		end
+		
+		player.power[gain.type].spells[gain.spellname] = player.power[gain.type].spells[gain.spellname] + amount
+	end
+end
+
+local MANA = 0
+
+local gain = {}
+
+local function SpellEnergize(timestamp, eventtype, srcGUID, srcName, srcFlags, dstGUID, dstName, dstFlags, ...)
+	-- Healing
+	local spellId, spellName, spellSchool, samount, powerType = ...
+	
+	heal.playerid = srcGUID
+	heal.playername = srcName
+	heal.spellid = spellId
+	heal.spellname = spellName
+	heal.amount = samount
+	heal.type = tonumber(powerType)
+	
+	Skada:FixPets(heal)
+	log_gain(Skada.current, gain)
+	log_gain(Skada.total, gain)
+end
+
+local function click_on_player(win, data, button)
+	if button == "LeftButton" then
+		playermod.playerid = data.id
+		playermod.name = data.label..L["'s Healing"]
+		win:DisplayMode(playermod)
+	elseif button == "RightButton" then
+		win:RightClick()
+	end
+end
+
+function mod:Update(win, set)
+	local nr = 1
+	local max = 0
+
+	for i, player in ipairs(set.players) do
+		if player.power[MANA] then
+			
+			local d = win.dataset[nr] or {}
+			win.dataset[nr] = d
+			
+			d.id = player.id
+			d.label = player.name
+			d.value = player.power[MANA]
+			d.valuetext = Skada:FormatNumber(player.power[MANA].amount)
+			d.class = player.class
+			
+			if player.power[MANA].amount > max then
+				max = player.power[MANA].amount
+			end
+			
+			nr = nr + 1
+		end
+	end
+	
+	win.metadata.maxvalue = max
+end
+
+local function spell_click(win, data, button)
+	if button == "RightButton" then
+		win:DisplayMode(mod)
+	end
+end
+
+-- Detail view of a player.
+function playermod:Update(win, set)
+	-- View spells for this player.
+		
+	local player = Skada:find_player(set, self.playerid)
+	local nr = 1
+	local max = 0
+	
+	if player then
+		
+		for spellname, spell in pairs(player.power[MANA].spells) do
+		
+			local d = win.dataset[nr] or {}
+			win.dataset[nr] = d
+			
+			d.id = spell.id
+			d.label = spell.name
+			d.value = spell.amount
+			d.valuetext = Skada:FormatNumber(spell.amount)..(" (%02.1f%%)"):format(spell.amount / player.power[MANA].amount * 100)
+			d.icon = select(3, GetSpellInfo(spell.id))
+			
+			if spell.amount > max then
+				max = spell.amount
+			end
+			
+			nr = nr + 1
+		end
+	end
+	
+	win.metadata.maxvalue = max
+end
+
+function mod:OnEnable()
+	mod.metadata		= {showspots = true, click = click_on_player}
+	playermod.metadata	= {click = spell_click}
+
+	Skada:RegisterForCL(SpellEnergize, 'SPELL_ENERGIZE', {src_is_interesting = true})
+	Skada:RegisterForCL(SpellEnergize, 'SPELL_PERIODIC_ENERGIZE', {src_is_interesting = true})
+
+	Skada:AddMode(self)
+end
+
+function mod:OnDisable()
+	Skada:RemoveMode(self)
+end
+
+function mod:AddToTooltip(set, tooltip)
+end
+
+function mod:GetSetSummary(set)
+	return Skada:FormatNumber(set.power[MANA])
+end
+
+-- Called by Skada when a new player is added to a set.
+function mod:AddPlayerAttributes(player)
+	if not player.power then
+		player.power = {}
+	end
+end
+
+-- Called by Skada when a new set is created.
+function mod:AddSetAttributes(set)
+	if not set.power then
+		set.power = {}
+	end
+end
