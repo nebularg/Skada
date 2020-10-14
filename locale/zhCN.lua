432f31cdeeb7e65a50123b49d94639d6de06dfb1
Kyle Buller <bullerk@gmail.com>
2020-10-14 18:30:02 -0500

    The great whitespace normalization

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,5 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Skada", "zhCN", false)
-
 if not L then return end
 
 --@localization(locale="zhCN", format="lua_additive_table")@
--- modules/Tweaks.lua
+++ modules/Tweaks.lua
@@ -1,4 +1,6 @@
--- Various silly tweaks needed to keep up with Blizzard's shenanigans. Not added to core because they may not be needed/relevant forever.
+local _, Skada = ...
+-- Various silly tweaks needed to keep up with Blizzard's shenanigans.
+-- Not added to core because they may not be needed/relevant forever.
 Skada:AddLoadableModule("Tweaks", "Various tweaks to get around deficiences and problems in the game's combat logs. Carries a small performance penalty.", function(Skada, L)
 	if Skada.db.profile.modulesBlocked.Tweaks then return end
 
