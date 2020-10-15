3ec840b9b9561a57b753553685c489505fb8dcc9
Kyle Buller <bullerk@gmail.com>
2020-10-14 20:15:47 -0500

    Ignore unlocalized phrases

--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -1,4 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Skada", "zhTW", false)
 if not L then return end
 
---@localization(locale="zhTW", format="lua_additive_table")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
