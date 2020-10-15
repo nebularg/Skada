15436a3d0fba9adf1e7b6d3c084386876c155a2a
Kyle Buller <bullerk@gmail.com>
2020-10-14 20:14:57 -0500

    Add esMX locale

---
+++ locale/esMX.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Skada", "esMX", false)
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore")@
